909ac0c53a9de22601abdc29b42adefbf9b578a3
Bhargav Bora <bhargavbora79@gmail.com>
2026-07-27 22:09:34 +0530

    problem solved

--- Arrays/stock1.js
+++ Arrays/stock1.js
@@ -12,3 +12,17 @@ Output:5
 Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6 – 1 = 5.
 */
 
+var maxProfit = function(prices) {
+    let maxProfit = 0;
+    let minPrice=prices[0];
+    for(let i=0;i<prices.length;i++){
+        if (prices[i]<minPrice){
+            minPrice=prices[i];
+    }
+    let profit = prices[i]-minPrice;
+    if(profit>maxProfit){
+    maxProfit =profit;
+    }
+    }
+    return maxProfit;
+};
