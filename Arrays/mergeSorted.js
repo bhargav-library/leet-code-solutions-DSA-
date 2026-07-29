681a6ad3049b7dbb76839e55cd84f1ce1fbae08b
Bhargav Bora <bhargavbora79@gmail.com>
2026-07-30 00:00:13 +0530

    problem solved

--- Arrays/mergeSorted.js
+++ Arrays/mergeSorted.js
@@ -12,3 +12,17 @@ Input:
 nums1 = [1,2,3,0,0,0], m = 3 nums2 = [2,5,6], n = 3
 
 Output: [1,2,2,3,5,6] */
+var merge = function(nums1, m, nums2, n) {
+    let p1 = m - 1;
+    let p2 = n - 1;
+  
+    for (let i = m + n - 1; i >= 0; i--) {
+      if (p2 < 0) break;
+  
+      if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+        nums1[i] = nums1[p1--];
+      } else {
+        nums1[i] = nums2[p2--];
+      }
+    }
+  };     
