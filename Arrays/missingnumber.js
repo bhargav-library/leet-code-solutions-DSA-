7e0616650b215ac5dd33499c2b39cda7a3d38f63
Bhargav Bora <bhargavbora79@gmail.com>
2026-08-01 14:28:12 +0530

    problem solved

---
+++ Arrays/missingnumber.js
@@ -0,0 +1,25 @@
+/*Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+Examples:
+Example 1:
+Input: nums = [3,0,1]
+
+Output: 2
+
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+Example 2:
+Input: nums = [0,1]
+
+Output: 2*/
+
+var missingNumber = function(nums) {
+      let n = nums.length;
+      let total_sum = (n * (n + 1)) / 2;
+      let sum_of_array = 0;
+  
+      for (let num of nums) {
+          sum_of_array += num;
+      }
+      return total_sum - sum_of_array;
+  };
