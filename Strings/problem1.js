cb9909ac42e3a7ba2e072f554ae1800dce152a13
Bhargav Bora <bhargavbora79@gmail.com>
2026-08-02 23:39:49 +0530

    added the problem statement

---
+++ Strings/problem1.js
@@ -0,0 +1,16 @@
+/*Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is defined as a maximal substring consisting of non-space characters only.
+
+Example:
+Input: s = “Hello World”
+Output: 5
+The last word is “World” with length 5.
+
+Input: s = ” fly me to the moon “
+Output: 4
+The last word is “moon” with length 4.
+
+Input: s = “luffy is still joyboy”
+Output: 6
+The last word is “joyboy” with length 6.*/
