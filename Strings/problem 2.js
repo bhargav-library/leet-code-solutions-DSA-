1f75773db246b85aee6c0ba242f7d4c95ba4bf69
Bhargav Bora <bhargavbora79@gmail.com>
2026-08-03 23:57:51 +0530

    added the problem statement

---
+++ Strings/problem 2.js
@@ -0,0 +1,13 @@
+/*You are given a 0-indexed array of strings words and a character x. Return an array of indices representing the words that contain the character x.
+
+The returned array can be in any order.
+
+Example:
+Input: words = [“leet”, “code”], x = “e”
+Output: [0, 1]
+
+Input: words = [“abc”, “bcd”, “aaaa”, “cbc”], x = “a”
+Output: [0, 2]
+
+Input: words = [“abc”, “bcd”, “aaaa”, “cbc”], x = “z”
+Output: []*/
