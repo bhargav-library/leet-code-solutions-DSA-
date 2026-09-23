9d852d888908c7806cd3c9c19edcdf2b1a6297a8
Bhargav Bora <bhargavbora79@gmail.com>
2026-09-23 22:03:20 +0530

    problem solved

--- Arrays/majorityElement.js
+++ Arrays/majorityElement.js
@@ -12,3 +12,19 @@ Example 2:
 
 Input: nums = [2,2,1,1,1,2,2]
 Output: 2*/
+
+function majorityElement(arr){
+    let candidate = null;
+    let count = 0;
+    for(let num of arr){
+        if(count===0){
+            candidate = num;
+        }
+       if(num===candidate){
+        count++
+       } else{
+        count--
+       }
+    }
+    return candidate;
+ }
